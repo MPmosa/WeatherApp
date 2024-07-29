fad6476fc4ab76af7200df2271ab68b53456bedd
MPmosa <169099768+MPmosa@users.noreply.github.com>
2024-07-29 21:02:39 +0200

    Added API

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#dayTemp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "to1f03b18d7d333882604afa441d6583";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchCity("Paris");
